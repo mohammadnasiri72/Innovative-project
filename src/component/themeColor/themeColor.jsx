a6846bca55dceea1c9e7287a7821fb8ed7d8f517
mohammadnasiri72 <127019973+mohammadnasiri72@users.noreply.github.com>
2024-03-05 12:17:07 -0800

    fgfg

--- src/component/themeColor/themeColor.jsx
+++ src/component/themeColor/themeColor.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import PalletColor from './palletColor'
 
 export default function ThemeColor() {
-    const [color , setColor] = useState('')
+    const [color , setColor] = useState('dfdf')
   return (
     <>
         <div className='h-screen flex justify-center items-center flex-col'>
